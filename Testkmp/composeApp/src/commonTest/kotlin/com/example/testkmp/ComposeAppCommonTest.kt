d9217229c57164708b858f48bfbe4e41387d9f71
Bodya <rybonka2007@gmail.com>
2026-02-27 15:17:01 +0200

    Add firts lab

---
+++ Testkmp/composeApp/src/commonTest/kotlin/com/example/testkmp/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.example.testkmp
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
